89fa9dba0a6f5810fb27dc0ff8dfaec35cf2d7f0
solidkenn <131536189+solidkenn@users.noreply.github.com>
2026-05-26 15:30:05 +0800

    Delete assets directory

--- assets/dark-mode-init.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  try {
-    if (localStorage.getItem('theme') === 'dark') {
-      document.documentElement.classList.remove('light');
-      document.documentElement.classList.add('dark');
-    }
-  } catch (e) {}
-})();
